93ad8eb11397e84c3dc30a604998d99f5af0f626
JaiSangle <sanglejai95@gmail.com>
2026-04-15 10:00:21 +0530

    Added a bit modularity and removed unnecessary moving and ownership transfers

--- src/main.rs
+++ src/main.rs
@@ -9,45 +9,52 @@ use image::{DynamicImage, ImageReader, GenericImageView};
 // 3. map brightness to a character
 // 4. writing the ascii to txt
 
-fn resize_image(image: DynamicImage) -> DynamicImage {
-    let res = image.resize(80, 40, image::imageops::FilterType::Nearest);
+fn resize_image(image: &DynamicImage) -> DynamicImage {
+    let res = image.resize(100, 50, image::imageops::FilterType::Nearest);
     res
 }
 
-fn convert_to_grayscale(image: DynamicImage) -> DynamicImage {
+fn convert_to_grayscale(image: &DynamicImage) -> DynamicImage {
     let gray = image.grayscale();
     gray
 }
 
-fn main() {
-    let mut _img = ImageReader::open("images/red-moon.png").unwrap().decode().unwrap();
-
-    // resizing image
-    let resized_image = resize_image(_img);
-
-    // converting to grayscale
-    let grayscale_image = convert_to_grayscale(resized_image);
-
-    // saving the grayscale just to check if it works
-    grayscale_image.save("images/grayscale.png").unwrap();
-
+fn convert_to_ascii(grayscale_image: &DynamicImage) -> String {
     // mapping character to brightness
-    let ramp = " .^*+=#&$@";
-    let mut ascii = Vec::new();
+    let ramp: Vec<char> = " .^*+=#&$@".chars().collect();
+    let ramp_len = ramp.len() as u32;
+    let mut output = String::new();
     let (width, height) = grayscale_image.dimensions();
     for row in 0..height {
         let mut cur = String::new();
         for col in 0..width {
             let pixel = grayscale_image.get_pixel(col, row);
             let brightness = pixel[0];
-            let idx = brightness as u32 * 9 / 255;
-            cur.push(ramp.chars().nth(idx as usize).unwrap_or(' '));
+            let idx = brightness as u32 * (ramp_len - 1) / 255;
+            cur.push(ramp[idx as usize]);
         }
-        cur.push_str("\n");
-        ascii.push(cur);
+        output.push_str(&cur);
     }
+    output
+}
 
-    let output = ascii.join("");
+
+fn main() {
+    // load the image using image reader
+    let img = ImageReader::open("images/red-moon.png").unwrap().decode().unwrap();
+
+    // resizing image
+    let resized_image = resize_image(&img);
+
+    // converting to grayscale
+    let grayscale_image = convert_to_grayscale(&resized_image);
+
+    // saving the grayscale just to check if it works
+    // grayscale_image.save("outputs/grayscale.png").unwrap();
+
+    // creating a file and writing the ascii to it
     let mut file = File::create("outputs/ascii.txt").unwrap();
-    file.write_all(output.as_bytes()).unwrap();
+    file.write_all(convert_to_ascii(&grayscale_image).as_bytes()).unwrap();
+
+    println!("completed");
 }
